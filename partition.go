e8a9a83a7b135bc57fd2a9a4390bfaa7e5edf2d4
AoyamaMidori <blueaoyamamidori@gmail.com>
2017-03-05 22:20:57 +0900

    Add zip object

--- partition.go
+++ partition.go
@@ -1,5 +1,38 @@
 package partition
 
+// Zip object is designed to gracefully return two ints. For use, see
+// example for Partition.
+type Zip struct {
+	// len(Left), len(Right) may be not equalvent.
+	Left, Right []int64
+	Pos         int
+}
+
+// Valid reports whether further iteration is valid or not.
+func (z *Zip) Valid() bool {
+	return z.Pos < len(z.Left) && z.Pos < len(z.Right)
+}
+
+// Next increments z.Pos by 1.
+func (z *Zip) Next() {
+	z.Pos++
+}
+
+// Fetch returns two elements pointed by z.Left and z.Right.
+func (z Zip) Fetch() (i int64, j int64) {
+	return z.Left[z.Pos], z.Right[z.Pos]
+}
+
+// First reports whether the iteration is first or not.
+func (z Zip) First() bool {
+	return z.Pos == 0 && len(z.Left) > 0 && len(z.Right) > 0
+}
+
+// Last reports whether the iteration is last or not.
+func (z Zip) Last() bool {
+	return z.Pos+1 == len(z.Left) || z.Pos+1 == len(z.Right)
+}
+
 // Partition partitions a slice with length x into n sub slices.
 // direction towards which partition goes is determined by the sign of
 // n, and the start of the partitioned slices is by x.
@@ -9,12 +42,12 @@ package partition
 //   n > 0: 0 -> x
 //   n < 0: x -> 0
 //
-func Partition(x, n int64) [][2]int64 {
+func Partition(x, n int64) Zip {
 	if x == 0 {
-		return [][2]int64{{0, 0}}
+		return lean([]int64{0, 0})
 	}
 	if n == 0 {
-		return [][2]int64{}
+		return Zip{}
 	}
 	var u, r, y int64
 	if n > 0 {
@@ -23,17 +56,17 @@ func Partition(x, n int64) [][2]int64 {
 			x = -x
 		}
 		u, r = x/n, x%n
-		s := make([][2]int64, n)
+		s := make([]int64, n+1)
 		for i := range s {
 			z := y + u
 			if r > 0 {
 				r--
 				z++
 			}
-			s[i] = [2]int64{y, z}
+			s[i] = y
 			y = z
 		}
-		return s
+		return lean(s)
 	}
 
 	n *= -1
@@ -43,15 +76,22 @@ func Partition(x, n int64) [][2]int64 {
 		x *= -1
 	}
 	u, r = x/n, x%n
-	s := make([][2]int64, n)
+	s := make([]int64, n+1)
 	for i := range s {
 		z := y - u
 		if r > 0 {
 			r--
 			z--
 		}
-		s[i] = [2]int64{y, z}
+		s[i] = y
 		y = z
 	}
-	return s
+	return lean(s)
+}
+
+func lean(s []int64) Zip {
+	return Zip{
+		Left:  s,
+		Right: s[1:],
+	}
 }
--- partition_test.go
+++ partition_test.go
@@ -3,10 +3,21 @@ package partition
 import "fmt"
 
 func ExamplePartition() {
-	fmt.Println(Partition(10, 3))
-	fmt.Println(Partition(10, -3))
-	fmt.Println(Partition(-10, 3))
-	fmt.Println(Partition(-10, -3))
+	PrintZip := func(x, n int64) {
+		fmt.Print("[")
+		for z := Partition(x, n); z.Valid(); z.Next() {
+			i, j := z.Fetch()
+			fmt.Print("[", i, " ", j, "]")
+			if !z.Last() {
+				fmt.Print(" ")
+			}
+		}
+		fmt.Println("]")
+	}
+	PrintZip(10, 3)
+	PrintZip(10, -3)
+	PrintZip(-10, 3)
+	PrintZip(-10, -3)
 	// Output:
 	// [[0 4] [4 7] [7 10]]
 	// [[10 6] [6 3] [3 0]]
